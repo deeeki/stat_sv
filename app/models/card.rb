3a8815867cdc4203f427180d3a5eb05bcc6853a4
deeeki <deeeki@gmail.com>
2017-11-21 02:17:08 +0900

    rails g model Card

---
+++ app/models/card.rb
@@ -0,0 +1,30 @@
+class Card
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :code, type: String
+  field :card_id, type: Integer
+  field :foil_card_id, type: Integer
+  field :card_set_id, type: Integer
+  field :card_name, type: String
+  field :is_foil, type: Integer
+  field :char_type, type: Integer
+  field :tribe_name, type: String
+  field :skill_disc, type: String
+  field :evo_skill_disc, type: String
+  field :cost, type: Integer
+  field :atk, type: Integer
+  field :life, type: Integer
+  field :evo_atk, type: Integer
+  field :evo_life, type: Integer
+  field :rarity, type: Integer
+  field :get_red_ether, type: Integer
+  field :use_red_ether, type: Integer
+  field :description, type: String
+  field :evo_description, type: String
+  field :cv, type: String
+  field :copyright, type: String
+  field :base_card_id, type: Integer
+  field :tokens, type: String
+  field :normal_card_id, type: Integer
+  belongs_to :clan
+end

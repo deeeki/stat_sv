bef015e83e353d5acb60f61869bfe217b50ee560
deeeki <deeeki@gmail.com>
2017-11-22 21:27:11 +0900

    Define hash of altanative cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -27,4 +27,32 @@ class Card
   field :tokens, type: String
   field :normal_card_id, type: Integer
   belongs_to :clan
+
+  ALTERNATIVES = {
+    'fof0I' => '61qeS', # エンシェントエルフ
+    'fp1Qo' => '62D2o', # 乙姫
+    'fpPrI' => '62bTS', # マーリン
+    'fpoFo' => '62zty', # フォルテ
+    'fqAgI' => '63MII', # ケルベロス
+    'fqZ4o' => '63kio', # クイーンヴァンパイア
+    'fqxVI' => '6477c', # ジャンヌダルク
+    'fsMZQ' => '6KolQ', # 対空射撃
+    'fskF2' => '5-Hb2', # タージ
+    'fsmhI' => '6LCtI', # ランサー
+    'ft9qg' => '6Lc0g', # 精神統一
+    'ftZyY' => '62xRs', # アジルス
+    'ftahQ' => '6M0tQ', # 誓いの一撃
+    'fttUY' => '6MJgY', # セイバーオルタ
+    'fuJ4A' => '6MlGA', # 絡みつく鎖
+    'fuglo' => '6N6y6', # バーサーカー
+    'fwHII' => '6DH3S', # 深き森の異形
+    'fwfio' => '69rKo', # アルベール
+    'fx27I' => '6E1uS', # 魔導の巨兵
+    'fxQXo' => '6EQIo', # 水竜神の巫女
+    'fxoyI' => '6A-aI', # ネフティス
+    'fyBMo' => '6FB7o', # ベルフェゴール
+    'fyZnI' => '6FZYS', # イージス
+    'f-sGI' => '6ADlI', # ドロシー
+    'f_Ego' => '6Ac9o', # インペリアルドラグーン
+  }
 end

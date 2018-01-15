51508dd8592cf3e2f7fdfdb7aba670f4fbca9a36
deeeki <deeeki@gmail.com>
2018-01-15 14:00:58 +0900

    Create only clans in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,7 @@ Clan.delete_all
   'ネクロマンサー' => 5,
   'ヴァンパイア' => 6,
   'ビショップ' => 7,
+  'ネメシス' => 8,
 }.each do |name, id|
   Clan.create(id: id, name: name)
-end
-
-Card.delete_all
-YAML.load_file(Rails.root.join('db/seeds/cards.yml')).each do |code, card|
-  Card.new(card).tap{|c|
-    c.id = c.card_id
-    c.code = code
-  }.save
-end
-
-Archetype.delete_all
-YAML.load_file(Rails.root.join('db/seeds/archetypes.yml')).each do |archetype|
-  Archetype.create(archetype)
 end

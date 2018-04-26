c528c88b7cc1818c5f7683a345be696c05abcd6b
deeeki <deeeki@gmail.com>
2018-04-27 05:53:35 +0900

    Detect clans from deck hash string

--- app/models/clan.rb
+++ app/models/clan.rb
@@ -3,4 +3,11 @@ class Clan
   include Mongoid::Timestamps
   field :name, type: String
   has_many :cards
+
+  class << self
+    def detect hash_str
+      clan_id = hash_str.split('/').last.split('.').second.to_i
+      Clan.find_by(id: clan_id)
+    end
+  end
 end

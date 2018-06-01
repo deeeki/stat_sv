766ebf1e3d0861ce2f3d8443633b67d0fa5eab0a
deeeki <deeeki@gmail.com>
2018-06-01 14:30:24 +0900

    Add DBN 2nd period

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,4 @@ end
 Period.find_or_create_by(card_set_code: :cgs, sequence: 1, started_on: Date.new(2017, 12, 29), ended_on: Date.new(2018, 1, 29))
 Period.find_or_create_by(card_set_code: :cgs, sequence: 2, started_on: Date.new(2018, 1, 30), ended_on: Date.new(2018, 3, 28))
 Period.find_or_create_by(card_set_code: :dbn, sequence: 1, started_on: Date.new(2018, 3, 29), ended_on: Date.new(2018, 5, 29))
+Period.find_or_create_by(card_set_code: :dbn, sequence: 2, started_on: Date.new(2018, 5, 30), ended_on: Date.new(2018, 6, 27))

a392ac18da172a9f3c8bc5aee1af15e68d49d317
deeeki <deeeki@gmail.com>
2018-06-30 07:31:27 +0900

    Add BOS 1st period

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,4 @@ Period.find_or_create_by(card_set_code: :cgs, sequence: 1, started_on: Date.new(
 Period.find_or_create_by(card_set_code: :cgs, sequence: 2, started_on: Date.new(2018, 1, 30), ended_on: Date.new(2018, 3, 28))
 Period.find_or_create_by(card_set_code: :dbn, sequence: 1, started_on: Date.new(2018, 3, 29), ended_on: Date.new(2018, 5, 29))
 Period.find_or_create_by(card_set_code: :dbn, sequence: 2, started_on: Date.new(2018, 5, 30), ended_on: Date.new(2018, 6, 27))
+Period.find_or_create_by(card_set_code: :bos, sequence: 1, started_on: Date.new(2018, 6, 28), ended_on: Date.new(2018, 9, 26))

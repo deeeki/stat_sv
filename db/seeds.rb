ee2be30c14fb82b06b022b47ebee06b3a143a0bc
deeeki <deeeki@gmail.com>
2018-07-18 21:43:18 +0900

    Add BOS 2nd period and rotation archetypes

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,4 +17,6 @@ Period.find_or_create_by(card_set_code: :cgs, sequence: 1, started_on: Date.new(
 Period.find_or_create_by(card_set_code: :cgs, sequence: 2, started_on: Date.new(2018, 1, 30), ended_on: Date.new(2018, 3, 28))
 Period.find_or_create_by(card_set_code: :dbn, sequence: 1, started_on: Date.new(2018, 3, 29), ended_on: Date.new(2018, 5, 29))
 Period.find_or_create_by(card_set_code: :dbn, sequence: 2, started_on: Date.new(2018, 5, 30), ended_on: Date.new(2018, 6, 27))
-Period.find_or_create_by(card_set_code: :bos, sequence: 1, started_on: Date.new(2018, 6, 28), ended_on: Date.new(2018, 9, 26))
+Period.find_or_create_by(card_set_code: :bos, sequence: 1, started_on: Date.new(2018, 6, 28), ended_on: Date.new(2018, 7, 17))
+Period.find_or_create_by(card_set_code: :bos, sequence: 2, started_on: Date.new(2018, 7, 18), ended_on: Date.new(2018, 8, 19))
+#Period.find_or_create_by(card_set_code: :bos, sequence: 3, started_on: Date.new(2018, 8, 20), ended_on: Date.new(2018, 9, 26))

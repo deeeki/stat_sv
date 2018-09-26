2cb1b1b55e51d5797cbb6a231bae6f5304e6a7d7
deeeki <deeeki@gmail.com>
2018-09-27 03:59:02 +0900

    Add OOT

--- app/models/period.rb
+++ app/models/period.rb
@@ -5,7 +5,7 @@ class Period
   field :sequence, type: Integer
   field :started_on, type: Date
   field :ended_on, type: Date
-  enumerize :card_set_code, in: [:cgs, :dbn, :bos], scope: true
+  enumerize :card_set_code, in: [:cgs, :dbn, :bos, :oot], scope: true
   has_many :archetypes
 
   class << self

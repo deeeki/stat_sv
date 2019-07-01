ad0ec97753cfa6048977d24146b0aeb56238d2f9
deeeki <deeeki@gmail.com>
2019-07-01 19:37:11 +0900

    ROG 1st period

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,3 +28,4 @@ Period.find_or_create_by(card_set_code: :alt, sequence: 2, started_on: Date.new(
 Period.find_or_create_by(card_set_code: :alt, sequence: 3, started_on: Date.new(2019, 2, 19), ended_on: Date.new(2019, 3, 27))
 Period.find_or_create_by(card_set_code: :str, sequence: 1, started_on: Date.new(2019, 3, 28), ended_on: Date.new(2019, 5, 20))
 Period.find_or_create_by(card_set_code: :str, sequence: 2, started_on: Date.new(2019, 5, 21), ended_on: Date.new(2019, 6, 26))
+Period.find_or_create_by(card_set_code: :rog, sequence: 1, started_on: Date.new(2019, 6, 27), ended_on: Date.new(2019, 7, 31))

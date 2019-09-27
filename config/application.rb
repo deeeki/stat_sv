f9f628e75760ed30fae15c7ceb03d1966794bfdb
deeeki <deeeki@gmail.com>
2019-09-27 21:47:23 +0900

    rails app:update

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ require "active_job/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
 require "sprockets/railtie"
@@ -23,8 +25,9 @@ module StatSv
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Don't generate system test files.
     config.generators.system_tests = nil

5f5b003de5396b8bd3cafe9dff1cd5b011f21312
deeeki <deeeki@gmail.com>
2020-04-20 21:46:45 +0900

    WUP 1st+2nd period

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,4 +36,7 @@ Period.find_or_create_by(card_set_code: :vec, sequence: 1, started_on: Date.new(
 Period.find_or_create_by(card_set_code: :vec, sequence: 2, started_on: Date.new(2019, 10, 29), ended_on: Date.new(2019, 11, 24))
 Period.find_or_create_by(card_set_code: :vec, sequence: 3, started_on: Date.new(2019, 11, 25), ended_on: Date.new(2019, 12, 12))
 Period.find_or_create_by(card_set_code: :vec, sequence: 4, started_on: Date.new(2019, 12, 13), ended_on: Date.new(2019, 12, 27))
-Period.find_or_create_by(card_set_code: :ucl, sequence: 1, started_on: Date.new(2019, 12, 28), ended_on: Date.new(2020, 1, 26))
+Period.find_or_create_by(card_set_code: :ucl, sequence: 1, started_on: Date.new(2019, 12, 28), ended_on: Date.new(2020, 2, 19))
+Period.find_or_create_by(card_set_code: :ucl, sequence: 2, started_on: Date.new(2020, 2, 20), ended_on: Date.new(2020, 3, 29))
+Period.find_or_create_by(card_set_code: :wup, sequence: 1, started_on: Date.new(2020, 3, 30), ended_on: Date.new(2020, 4, 1))
+Period.find_or_create_by(card_set_code: :wup, sequence: 2, started_on: Date.new(2020, 4, 2), ended_on: Date.new(2020, 4, 27))

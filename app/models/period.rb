32f5faf4afc372b43d1a85936a6904e9b41741b2
deeeki <deeeki@gmail.com>
2021-01-10 15:31:56 +0900

    Add ETA

--- app/models/period.rb
+++ app/models/period.rb
@@ -5,7 +5,7 @@ class Period
   field :sequence, type: Integer
   field :started_on, type: Date
   field :ended_on, type: Date
-  enumerize :card_set_code, in: [:cgs, :dbn, :bos, :oot, :alt, :str, :rog, :vec, :ucl, :wup, :foh, :sor], scope: true
+  enumerize :card_set_code, in: [:cgs, :dbn, :bos, :oot, :alt, :str, :rog, :vec, :ucl, :wup, :foh, :sor, :eta], scope: true
   has_many :archetypes
 
   class << self
